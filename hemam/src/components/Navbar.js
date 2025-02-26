5fd2ebaa950995adcc7599616eca3cc0a45e9a80
Malek Alarmash <81653238+Malekalarmash@users.noreply.github.com>
2025-02-26 08:50:56 -0500

    fixing the navbar

--- hemam/src/components/Navbar.js
+++ hemam/src/components/Navbar.js
@@ -5,22 +5,21 @@ const Navbar = () => {
         imageUrl: '../images/logo.svg',
     };
     return (
-        <nav className="flex justify-center p-4 bg-transparent">     
-            <ul className="flex space-x-6 rtl:space-x-reverse">
-            <li><a href="#blog" className="text-right mr-6">تواصل معنا</a></li>
+    <nav className="flex p-4 bg-transparent place-content-between w-full padding:8px">   
+            <button className="signup-btn">تسجيل الدخول</button>    
+        <ul className="flex space-x-6 rtl:space-x-reverse item-center">
+            <li><a href="#blog" className="text-right">تواصل معنا</a></li>
             <li><a href="#contact" className="text-right">المتصدرين</a></li>
             <li><a href="#portfolio" className="text-right">قصص نجاح</a></li>
             <li><a href="#organizations" className="text-right">المنظمات</a></li>
             <li><a href="#opportunities" className="text-right">استكشف الفرص</a></li>
-            <li><a href="#home" className="text-right">الصفحة الرئيسية </a></li>
+            <li><a href="#home" className="text-right">الصفحة الرئيسية </a></li>       
+          </ul>
             <div className='flex justify-right'>
                 <img src={logo.imageUrl} alt="Hemam Logo" className="w-66px h-50px" />
-            </div>
-
-               
-                
-            </ul>
-        </nav>
+            </div>  
+    </nav>
+        
     );
 };
 export default Navbar;
